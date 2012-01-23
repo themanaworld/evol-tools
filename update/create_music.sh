413481aae53a25e49f591b611e806ce443ad1832
Andrei Karas <akaras@inbox.ru>
2012-01-23 23:07:33 +0300

    update: Add missing files.

---
+++ update/create_music.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright (C) 2011-2012  Evol Online
+# Author: Andrei Karas (4144)
+
+dir=`pwd`
+
+rm adler32
+gcc -lz adler32.c -o adler32
+
+mkdir files
+mkdir upload
+
+rm files/music.zip
+cd ../../gittorious/evol-music
+find -iregex ".+[.]\(ogg\)" -printf "%P\n" | zip -@ ../../privtools/update/files/music.zip
+git log --pretty=oneline -n 1 | awk '{print $1}' >../../privtools/update/musiccommit.txt
+
+cd $dir/files
+sum=`../adler32 1 music.zip`
+
+echo "    <update type=\"music\" required=\"no\" file=\"music.zip\" hash=\"${sum}\" description=\"Evol music\" />" >> xml_header.txt
+
+cp xml_header.txt resources.xml
+cat xml_footer.txt >>resources.xml
+
+cp music.zip ../upload/
+cp resources.xml ../upload/
+cp ../news.txt ../upload/
---
+++ update/update_music.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Copyright (C) 2010-2012  Evol Online
+# Author: Andrei Karas (4144)
+
+dir=`pwd`
+
+rm adler32
+gcc -lz adler32.c -o adler32
+
+mkdir files
+mkdir upload
+
+previous=`cat musiccommit.txt`
+
+cd ../../gittorious/evol-music
+head=`git log --pretty=oneline -n 1 | awk '{print $1}'`
+u1=`echo ${previous} | cut -c 1-7`
+u2=`echo ${head} | cut -c 1-7`
+git log --name-status ${previous}..${head} | awk '/^(A|M)\t/ {print $2}' | \
+    grep -e "[.]\(ogg\)" | sort | uniq | \
+    xargs zip -9 -r ../../privtools/update/files/music-${u1}..${u2}.zip
+
+cd $dir/files
+if [ -f evol-${u1}..${u2}.zip ]; then
+    mv ../muciscommit.txt ../muciscommit_old.txt
+    echo ${head} >../muciscommit.txt
+    sum=`../adler32 1 music-${u1}..${u2}.zip`
+    echo "    <update type=\"music\" required=\"no\" file=\"music-${u1}..${u2}.zip\" hash=\"${sum}\" description=\"Evol music\" />" >> xml_header.txt
+    cp xml_header.txt resources.xml
+    cat xml_footer.txt >>resources.xml
+
+    cp music-${u1}..${u2}.zip ../upload/
+    cp resources2.txt ../upload/
+    cp resources.xml ../upload/
+    cp ../news.txt ../upload
+fi
