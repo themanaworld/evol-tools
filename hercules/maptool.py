a8151acf92fba0e0c8b09e7f04dbb7a4bc89834a
Andrei Karas <akaras@inbox.ru>
2014-09-24 11:41:39 +0300

    hercules: add pets.xml generation.

--- hercules/maptool.py
+++ hercules/maptool.py
@@ -35,6 +35,8 @@ def detectCommand():
         return "monsterstoxml"
     elif sys.argv[0][-20:] == "/mercenariestoxml.py":
         return "mercenariestoxml"
+    elif sys.argv[0][-13:] == "/petstoxml.py":
+        return "petstoxml"
     return "help"
 
 def makeDir(path):
@@ -394,6 +396,27 @@ def convertMercenaries():
             data = data + tpl.format(mercenaryId, mercenarySprite)
     saveFile(destDir + "mercenaries.xml", mercenaries.format(data))
 
+def convertPets():
+    destDir = "clientdata/"
+    templatesDir = "templates/"
+    petsDbFile = "serverdata/db/pet_db.txt"
+    fieldsSplit = re.compile(",")
+    makeDir(destDir)
+    tpl = readFile(templatesDir + "pet.tpl")
+    pets = readFile(templatesDir + "pets.xml")
+    data = ""
+    petSprite = "<sprite>monsters/tortuga.xml</sprite>";
+    with open(petsDbFile, "r") as f:
+        for line in f:
+            if line == "" or line[0:2] == "//":
+                continue
+            rows = fieldsSplit.split(line)
+            if len(rows) < 9:
+                continue
+            petId = rows[0]
+            data = data + tpl.format(petId, petSprite)
+    saveFile(destDir + "pets.xml", pets.format(data))
+
 
 def readMapCache(path, cmd):
     if cmd == "help":
@@ -409,6 +432,9 @@ def readMapCache(path, cmd):
         return
     elif cmd == "mercenariestoxml":
         convertMercenaries();
+        return
+    elif cmd == "petstoxml":
+        convertPets();
         return
 
     with open(path, "rb") as f:
