6f8dd2e9450befa9632ebcee1086c2095ee0aeae
Andrei Karas <akaras@inbox.ru>
2014-09-27 21:56:02 +0300

    hercules: add support for converting skils.xml

--- hercules/maptool.py
+++ hercules/maptool.py
@@ -39,6 +39,8 @@ def detectCommand():
         return "petstoxml"
     elif sys.argv[0][-21:] == "/homunculusestoxml.py":
         return "homunculusestoxml"
+    elif sys.argv[0][-15:] == "/skillstoxml.py":
+        return "skillstoxml"
     return "help"
 
 def makeDir(path):
@@ -451,6 +453,31 @@ def convertHomunculuses():
             data = data + tpl.format(homunculusId, homunculusSprite)
     saveFile(destDir + "homunculuses.xml", homunculuses.format(data))
 
+def convertSkillsToXml():
+    destDir = "clientdata/"
+    templatesDir = "templates/"
+    skillsDbFile = "serverdata/db/re/skill_db.txt"
+    fieldsSplit = re.compile(",")
+    makeDir(destDir)
+    tpl = readFile(templatesDir + "skill.tpl")
+    skills = readFile(templatesDir + "skills.xml")
+    data = ""
+    with open(skillsDbFile, "r") as f:
+        for line in f:
+            if line == "" or line[0:2] == "//":
+                continue
+            rows = fieldsSplit.split(line)
+            if len(rows) < 9:
+                continue
+            skillId = rows[0]
+            name = rows[15].strip()
+            description = rows[16].strip()
+            idx = description.find ("//")
+            if idx > 1:
+                description = description[:idx]
+            data = data + tpl.format(skillId, name, description)
+    saveFile(destDir + "skills.xml", skills.format(data))
+
 
 def readMapCache(path, cmd):
     if cmd == "help":
@@ -472,6 +499,9 @@ def readMapCache(path, cmd):
         return
     elif cmd == "homunculusestoxml":
         convertHomunculuses();
+        return
+    elif cmd == "skillstoxml":
+        convertSkillsToXml();
         return
 
     with open(path, "rb") as f:
