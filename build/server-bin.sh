d4a9e360994135b2dc490a472feab6bd5da363af
Andrei Karas <akaras@inbox.ru>
2014-11-12 22:09:40 +0300

    server-bin: build also hercules-plugin.

--- build/server-bin.sh
+++ build/server-bin.sh
@@ -5,9 +5,21 @@ make clean
 ./build.sh static
 
 if [[ $? == 0 ]]; then
-    cp char-server ../server-bin
-    cp login-server ../server-bin
-    cp map-server ../server-bin
+    cp -f char-server ../server-bin
+    cp -f login-server ../server-bin
+    cp -f map-server ../server-bin
+else
+    echo Build failed
+    exit 1
+fi
+
+cd src/evol
+make clean
+./build.sh
+
+if [[ $? == 0 ]]; then
+    mkdir ../../../server-local/plugins
+    cp -f build/src/.libs/*.so ../../../server-local/plugins
     echo Build success
 else
     echo Build failed
