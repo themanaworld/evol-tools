899eaf941effe56c95ddf23dc358e0fceed4c017
Andrei Karas <akaras@inbox.ru>
2014-11-16 17:19:17 +0300

    Add script and configuration for launch manaplus and connect to local evol server.
    New script: manaplus/connect_local_server.sh

---
+++ manaplus/connect_local_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export MANAPLUS="../../manaplus/src/manaplus"
+if [ -f "$MANAPLUS" ]; then
+    echo "Starting local manaplus"
+else
+    echo "Starting system manaplus"
+    export MANAPLUS="manaplus"
+fi
+
+${MANAPLUS} -d ../../client-data evol.manaplus
