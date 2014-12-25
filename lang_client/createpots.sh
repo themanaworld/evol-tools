0044bd039a020b1eea1e23d2e6e8b74e8813157c
Andrei Karas <akaras@inbox.ru>
2014-12-25 15:02:36 +0300

    lang_client: fix path to client data.

--- lang_client/createpots.sh
+++ lang_client/createpots.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function its {
-    itstool -o ../../evol-tools/lang_client/pot/$1.pot $1.xml
+    itstool -o ../tools/lang_client/pot/$1.pot $1.xml
 }
 
 DIR=`pwd`
-cd ../../gittorious/clientdata-beta/
+cd ../../client-data
 
 
 its items
@@ -15,10 +15,12 @@ its itemcolors
 its monsters
 its quests
 its pets
+its npcs
+its deadmessages
 
 cd $DIR
 
-itstool -o pot/manaplus_emotes.pot tmp/manaplus_emotes.xml
-itstool -o pot/deadmessages.pot tmp/deadmessages.xml
+itstool -o pot/manaplus_emotes1.pot tmp/manaplus_emotes.xml
+itstool -o pot/deadmessages1.pot tmp/deadmessages.xml
 
 msgcat pot/*.pot > clientdata-beta.pot
