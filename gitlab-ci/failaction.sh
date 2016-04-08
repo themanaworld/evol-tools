685ae04d1cc040bb333e436f8d4e4a23c7b13704
Andrei Karas <akaras@inbox.ru>
2016-04-08 21:43:31 +0300

    gitlab-ci: copy possible error into pastebin and report it in irc.

--- gitlab-ci/failaction.sh
+++ gitlab-ci/failaction.sh
@@ -5,6 +5,7 @@ export server="irc.freenode.net"
 export channel="#evol-dev"
 export nick="evolbuildbot"
 export buildid="$(cat clientdata/.shared/buildid.log)"
+export error="$(cat clientdata/.shared/error.log)"
 
 export msg="Build failed. See https://gitlab.com/evol/clientdata/builds/${buildid}"
 
@@ -19,6 +20,11 @@ echo "/j ${channel}" > "${path}/${server}/in"
 sleep 10s
 
 echo ${msg} >${path}/${server}/${channel}/in
+if [[ -n "${error}" ]]; then
+    sleep 2s
+    export LINK=$(pastebinit -b http://paste.ubuntu.com/ clientdata/.shared/error.log)
+    echo "Error log: ${LINK}" >${path}/${server}/${channel}/in
+fi
 sleep 3s
 
 rm -rf clientdata/.shared/buildid.log
