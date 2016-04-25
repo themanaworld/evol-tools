54276f37f8be6810d0c665dae2ce6be61dd50665
Andrei Karas <akaras@inbox.ru>
2016-04-25 17:03:52 +0300

    gitlab-ci: fail client data builds if some thing bad detected.

--- gitlab-ci/clientdata.sh
+++ gitlab-ci/clientdata.sh
@@ -33,14 +33,16 @@ if [[ -n "${RES}" ]]; then
     echo "xml check failed"
     echo "xml check failed" >../../clientdata/shared/error.log
     echo ${RES} >>../../clientdata/shared/error.log
-    exit 0
+    cat ../../clientdata/shared/error.log
+    exit 1
 fi
 
 echo >../../clientdata/shared/error.log
 ./testxml.py silent >../../clientdata/shared/error.log
 if [ "$?" != 0 ]; then
     echo "test xml error"
-    exit 0
+    cat ../../clientdata/shared/error.log
+    exit 1
 fi
 
 echo >../../clientdata/shared/error.log
