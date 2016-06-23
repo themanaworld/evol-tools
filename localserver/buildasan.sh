b46da2bec2c96f85727f67cbd100f5552aa71cb5
Andrei Karas <akaras@inbox.ru>
2016-06-23 19:25:33 +0300

    localserver: add script for clean from old build files.

--- localserver/buildasan.sh
+++ localserver/buildasan.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 export DIR=`pwd`
-rm -rf ../../server-plugin/build/
-mkdir ../../server-plugin/build/
-cd ${DIR}/../../server-code/
-make clean
+./clean.sh
 ./build.sh
---
+++ localserver/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf ../../server-plugin/build/
+mkdir ../../server-plugin/build/
+cd ../../server-code/
+make clean
